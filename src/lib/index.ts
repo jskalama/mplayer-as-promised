c8931426700f4663b9e57b39b6aebed7558cc96f
no <no>
2018-01-23 08:57:04 +0200

    fix for code review

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,60 +1,9 @@
 import { MPlayerManager } from './mplayerManager'
+import { Metadata, parseMetadata } from './parseUtils'
 
 const DEFAULT_OP_TIMEOUT = 2000;
 const OPEN_OP_TIMEOUT = 8000;
 
-const parseKeyValueList = (data: string): { [id: string]: string } => {
-  return data
-    .split(',')
-    .reduce(
-      (
-        res: { [id: string]: string },
-        item: string,
-        i: number,
-        arr: Array<string>
-      ) => {
-        if (!(i % 2)) {
-          res[item.toLowerCase()] = (arr[i + 1] || '').trim();
-        }
-        return res;
-      },
-      {}
-    );
-};
-
-export type Metadata = Partial<{
-  title: string;
-  artist: string;
-  album: string;
-  year: number;
-  comment: string;
-  track: number;
-  genre: string;
-}>;
-
-const parseMetadata = (data: string): Metadata => {
-  if (data === '(null)') {
-    return {
-      title: null,
-      artist: null,
-      album: null,
-      year: null,
-      comment: null,
-      track: null,
-      genre: null
-    };
-  }
-  const raw = parseKeyValueList(data);
-  return {
-    title: raw.title,
-    artist: raw.artist,
-    album: raw.album,
-    year: raw.year ? parseInt(raw.year, 10) : null,
-    comment: raw.comment,
-    track: raw.track ? parseInt(raw.track, 10) : null,
-    genre: raw.genre
-  };
-};
 
 /**
  * MPlayerMediaItem
@@ -119,11 +68,11 @@ export class MPlayerMediaItem {
   }
 
   /**
-   * getMeta
-   * 
+   * getMetadata
+   *
    * @returns a promise that resolves with the metadata
    */
-  public getMeta(): Promise<Metadata> {
+  public getMetadata(): Promise<Metadata> {
     if (!this.mplayer) {
       return Promise.reject('Not in a valid state');
     }
